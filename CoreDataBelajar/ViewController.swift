8a57ca2ef13a23bcc7fdea6cf188e57f7b3aa0ae
Ricky Erdiansyah <erdiansyah.ricky@gmail.com>
2019-06-24 09:58:46 +0700

    Initial Commit

---
+++ CoreDataBelajar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataBelajar
+//
+//  Created by Ricky Erdiansyah on 24/06/19.
+//  Copyright © 2019 Ricky Erdiansyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
